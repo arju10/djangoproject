d89a27282475499ebd5cba512d5fafdaaf98b8df
arju10 <arjuarju71@gmail.com>
2024-12-08 11:38:23 +0600

    Create product variety model, add pillow for image

--- djangoproject/products/models.py
+++ djangoproject/products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class ProductsVariety(models.Model):
+    PRODUCT_TYPE_CHOICE = [
+        ('MOUSE', 'Mouse'),
+        ('KEYBOARD', 'Keyboard'),
+        ('RAM', 'RAM'),
+        ('HARD-DISK', 'Hard Disk'),
+        ('WINDOWS 11', 'Windows 11'),
+        ('WINDOWS 10', 'Windows 10'),
+        ('LAPTOP', 'Laptop'),
+        ('DESKTOP', 'Desktop'),
+        ('MOBILE CHARGER', 'Mobile Charger'),
+        ('LAPTOP CHARGER', 'Laptop Charger'),
+    ]
+
+    name = models.CharField(max_length=100) 
+    image = models.ImageField(upload_to='products/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=20, choices=PRODUCT_TYPE_CHOICE)  
+
+    def __str__(self):
+        return self.name
