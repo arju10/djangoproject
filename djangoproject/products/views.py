d7b4a8f1d7e1a57e13bf4d5153219f5e30db1694
arju10 <arjuarju71@gmail.com>
2024-12-08 12:01:26 +0600

    All products show in frontend

--- djangoproject/products/views.py
+++ djangoproject/products/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import ProductsVariety
 
 # Create your views here.
 def all_products(request):
-    return render(request, 'products/all_products.html')
+    products = ProductsVariety.objects.all()
+    return render(request, 'products/all_products.html', {'products':products})
 
 def order(request):
     return render(request, 'products/all_products.html')
